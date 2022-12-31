dcedd035ab683db11a6ed049d509c8687d131720
javinkurt <110312220+javinkurt@users.noreply.github.com>
2022-12-31 12:44:40 +0300

    Create bio.js

---
+++ bio.js
@@ -0,0 +1,25 @@
+let {Module} = require('../main');
+/*
+Credit: javinkurt/#
+Module({
+pattern: 'autobio ?(.*)',
+fromMe: false
+*/
+let on_aano = false
+Module({on:"text",fromMe:false},async (m)=>{
+if (on_aano=== true || on_aano === null) return;
+if (m.message.toLowerCase() == "autobio off") {
+on_aano = null
+clearInterval(bioSetter)
+await m.send("Autobio devre dışı, plugini kaldır!")
+}
+on_aano = true
+async function setAbout(){
+let status = "📆 "+new Date().toLocaleDateString()+" ⌚ "+new Date().toLocaleTimeString()+" OTO BİO ⚡"
+await m.client.updateProfileStatus(status)
+return "Done"
+}
+m.jid = m.client.user.id
+await m.send("OTO bio aktif!");
+let bioSetter = setInterval(setAbout,9999)
+})
